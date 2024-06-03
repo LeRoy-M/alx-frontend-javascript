abeea32cf5c5bbaf0d450e2e3042ad49a1ff24d7
LeRoy-M <lmwasela@gmail.com>
2024-06-03 13:00:37 +0300

    2. Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  obj = {
-    status: 200,
-    body: 'success'
-  };
-
-  promise.then((obj) => {
-    return (obj);
-  })
-  .catch ((err) => {
-    return (err);
-  })
-  .finally(() => {
-    ('Got a response from the API');
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
